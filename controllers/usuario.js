deefbce28c002015af7f8c766a5b698cd8cf1c83
scmiguel <miguelsouzacintra@gmail.com>
2022-11-18 14:19:36 +0000

    18-11/3

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -4,7 +4,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
-const Usuario = require('../models/usuario')
+const {Usuario} = require('../models/usuario')
 
 const controller = {}       // Objeto vazio
 
